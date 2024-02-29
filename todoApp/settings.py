c6c2dcf73357be6ef764495e45a51ca365fb5fc6
viveklav <161590787+viveklav@users.noreply.github.com>
2024-02-29 17:05:29 +0530

    Update settings.py

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['16.16.90.45']
+ALLOWED_HOSTS = ['16.16.169.191']
 
 
 # Application definition
